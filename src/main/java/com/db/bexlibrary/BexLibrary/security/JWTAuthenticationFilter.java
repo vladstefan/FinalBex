0e41be96d6260801dfa997746575d9e27b17baae
Alexandra Preda <predaalexanddra@gmail.com>
2018-05-15 09:51:48 +0300

    .idea deleted

--- src/main/java/com/db/bexlibrary/BexLibrary/security/JWTAuthenticationFilter.java
+++ src/main/java/com/db/bexlibrary/BexLibrary/security/JWTAuthenticationFilter.java
@@ -1,27 +1,29 @@
 package com.db.bexlibrary.BexLibrary.security;
 
+import static com.db.bexlibrary.BexLibrary.security.SecurityConstants.EXPIRATION;
+import static com.db.bexlibrary.BexLibrary.security.SecurityConstants.HEADER_STRING;
+import static com.db.bexlibrary.BexLibrary.security.SecurityConstants.SECRET;
+import static com.db.bexlibrary.BexLibrary.security.SecurityConstants.TOKEN_PREFIX;
+
 import com.db.bexlibrary.BexLibrary.entities.AppUser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import java.io.IOException;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
-
-import static com.db.bexlibrary.BexLibrary.security.SecurityConstants.*;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
---
+++ src/main/webapp/react-bootstrap/bootstrap-app/src/Dashboard.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton';
+import {
+    Table,
+    TableBody,
+    TableHeader,
+    TableHeaderColumn,
+    TableRow,
+    TableRowColumn,
+} from 'material-ui/Table';
+
+export default class Dashboard extends Component {
+    state = {
+        selected: [1],
+        showCheckboxes:false
+    };
+
+    isSelected = (index) => {
+        return this.state.selected.indexOf(index) !== -1;
+    };
+
+    handleRowSelection = (selectedRows) => {
+        this.setState({
+            selected: selectedRows,
+        });
+    };
+
+    render() {
+        return (
+            <MuiThemeProvider>
+            <Table onRowSelection={this.handleRowSelection}>
+                <TableHeader adjustForCheckbox={this.state.showCheckboxes} displaySelectAll={this.state.showCheckboxes}>
+
+                    <TableRow>
+                        <TableHeaderColumn>ID</TableHeaderColumn>
+                        <TableHeaderColumn>Name</TableHeaderColumn>
+                        <TableHeaderColumn>Status</TableHeaderColumn>
+                        <TableHeaderColumn></TableHeaderColumn>
+                    </TableRow>
+                </TableHeader>
+                <TableBody displayRowCheckbox={this.state.showCheckboxes}>
+                    <TableRow selected={this.isSelected(0)}>
+                        <TableRowColumn>1</TableRowColumn>
+                        <TableRowColumn>John Smith</TableRowColumn>
+                        <TableRowColumn>Employed</TableRowColumn>
+                        <FlatButton label="RETURN"/>
+                    </TableRow>
+                    <TableRow selected={this.isSelected(1)}>
+                        <TableRowColumn>2</TableRowColumn>
+                        <TableRowColumn>Randal White</TableRowColumn>
+                        <TableRowColumn>Unemployed</TableRowColumn>
+                    </TableRow>
+                    <TableRow selected={this.isSelected(2)}>
+                        <TableRowColumn>3</TableRowColumn>
+                        <TableRowColumn>Stephanie Sanders</TableRowColumn>
+                        <TableRowColumn>Employed</TableRowColumn>
+                    </TableRow>
+                    <TableRow selected={this.isSelected(3)}>
+                        <TableRowColumn>4</TableRowColumn>
+                        <TableRowColumn>Steve Brown</TableRowColumn>
+                        <TableRowColumn>Employed</TableRowColumn>
+                    </TableRow>
+                </TableBody>
+            </Table>
+            </MuiThemeProvider>
+        );
+    }
+}
